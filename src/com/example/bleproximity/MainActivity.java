26030d625535bf2039bb1bf72c2ed4c8e8ee6ba2
Sam Damen <damen.93@hotmail.com>
2014-04-06 22:58:06 +1000

    Radius Networks library
    Started working with the provided iBeacon library. Gives simplicity but
    not depth of understanding, may need to write my own

--- src/com/example/bleproximity/MainActivity.java
+++ src/com/example/bleproximity/MainActivity.java
@@ -1,28 +1,69 @@
 package com.example.bleproximity;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends Activity {
+import com.radiusnetworks.ibeacon.IBeaconConsumer;
+import com.radiusnetworks.ibeacon.IBeaconManager;
+import com.radiusnetworks.ibeacon.MonitorNotifier;
+import com.radiusnetworks.ibeacon.Region;
+
+public class MainActivity extends Activity implements IBeaconConsumer {
+	
+	protected static final String TAG = "RangingActivity";
+	private IBeaconManager iBeaconManager = IBeaconManager.getInstanceForApplication(this);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		setContentView(R.layout.activity_monitoring);
+		verifyBluetooth();
+		iBeaconManager.bind(this);		
 	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		iBeaconManager.unBind(this);		
+	}
+	
+	
+	
+	@Override
+	public void onIBeaconServiceConnect() {
+		iBeaconManager.setMonitorNotifier(new MonitorNotifier() {
+			@Override
+			public void didEnterRegion(Region region) {
+				Log.i(TAG, "Saw iBeacon for first time");
+			}
+			
+			@Override
+			public void didExitRegion(Region region) {
+				Log.i(TAG, "No longer see iBeacon");
+			}
+			
+			@Override
+			public void didDetermineStateForRegion(int state, Region region) {
+				Log.i(TAG, "Switched from seeing/not seeing iBeacons: " + state);
+			}
+		});
+	
+	
+	try {
+		iBeaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+	} catch (RemoteException e) { }
+	
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
