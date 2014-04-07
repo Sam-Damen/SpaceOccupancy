7b81b7116f44cdbb35cc2b2d81e0e1231c7ea5a6
Sam Damen <damen.93@hotmail.com>
2014-04-07 20:34:40 +1000

    Working BLE Scanner
    Decided to write own BLE scanner to better understand the Android
    functions. Tested with a Raspberry Pi iBeacon to show a list of the
    device address and rssi.

---
+++ src/com/example/bleproximity/BLEBeacon.java
@@ -0,0 +1,34 @@
+package com.example.bleproximity;
+
+
+/*
+ * Simple type to define beacon address and rssi
+ */
+class BLEBeacon {
+	
+	private int mSignal;
+	private String mAddress;
+	
+	public BLEBeacon(String deviceAddr, int rssi) {
+		mSignal = rssi;
+		mAddress = deviceAddr;		
+	}
+	
+	public int getSignal() {
+		return mSignal;
+	}
+	
+	public String getAddress() {
+		return mAddress;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %ddBm", mAddress, mSignal);
+		
+	}
+	
+	
+	
+	
+}
--- src/com/example/bleproximity/MainActivity.java
+++ src/com/example/bleproximity/MainActivity.java
@@ -1,70 +1,194 @@
 package com.example.bleproximity;
 
+import java.util.HashMap;
+
 import android.app.Activity;
-import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.RemoteException;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import com.radiusnetworks.ibeacon.IBeaconConsumer;
-import com.radiusnetworks.ibeacon.IBeaconManager;
-import com.radiusnetworks.ibeacon.MonitorNotifier;
-import com.radiusnetworks.ibeacon.Region;
-
-public class MainActivity extends Activity implements IBeaconConsumer {
+public class MainActivity extends Activity implements BluetoothAdapter.LeScanCallback {
 	
-	protected static final String TAG = "RangingActivity";
-	private IBeaconManager iBeaconManager = IBeaconManager.getInstanceForApplication(this);
-
+	protected static final String TAG = "BeaconActivity";
+	private BluetoothAdapter mBLEAdapter;
+	private HashMap<String, BLEBeacon> mBeacons;
+	private BeaconListAdapter mAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//Use a cyclic progress to show scan is happening
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		setProgressBarIndeterminate(true);
 		
-		setContentView(R.layout.activity_monitoring);
-		verifyBluetooth();
-		iBeaconManager.bind(this);		
+		/*
+		 * Display beacons in list
+		 */		
+		ListView list = new ListView(this);
+		//Custom adapter to show beacon attributes
+		mAdapter = new BeaconListAdapter(this);
+		list.setAdapter(mAdapter);
+		setContentView(list);
+		
+		/*
+		 * Get the Bluetooth Adapter
+		 */
+		BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+		mBLEAdapter = manager.getAdapter();
+		
+		//Organises found Beacons by their address, only allow 1 instance of each beacon found
+		mBeacons = new HashMap<String, BLEBeacon>();
+		
 	}
 	
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		iBeaconManager.unBind(this);		
+	protected void onResume() {
+		super.onResume();
+		/*
+		 * Check and ensure Bluetooth is enabled
+		 */
+		if (mBLEAdapter == null || ! mBLEAdapter.isEnabled()) {
+			Intent enBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivity(enBluetoothIntent);
+			finish();
+			return;
+		}
+		
+		/*
+		 * Check BLE is supported
+		 */
+		if (! getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+			//Make a toast to tell no BLE support
+			finish();
+			return;
+		}
+		
+		//Begin a Scan for Devices
+		startScan();
 	}
-	
-	
 	
 	@Override
-	public void onIBeaconServiceConnect() {
-		iBeaconManager.setMonitorNotifier(new MonitorNotifier() {
-			@Override
-			public void didEnterRegion(Region region) {
-				Log.i(TAG, "Saw iBeacon for first time");
-			}
-			
-			@Override
-			public void didExitRegion(Region region) {
-				Log.i(TAG, "No longer see iBeacon");
-			}
-			
-			@Override
-			public void didDetermineStateForRegion(int state, Region region) {
-				Log.i(TAG, "Switched from seeing/not seeing iBeacons: " + state);
-			}
-		});
-	
-	
-	try {
-		iBeaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
-	} catch (RemoteException e) { }
-	
+	protected void onPause() {
+		super.onPause();
+		//Stop scanning
+		mHandler.removeCallbacks(mStopRun);
+		mHandler.removeCallbacks(mStartRun);
+		mBLEAdapter.stopLeScan(this);
 	}
 	
-
+	private Runnable mStopRun = new Runnable() {
+		@Override
+		public void run() {
+			stopScan();
+		}
+	};
+	
+	private Runnable mStartRun = new Runnable() {
+		@Override
+		public void run() {
+			startScan();
+		}
+	};
+	
+	/*
+	 * These two functions alternate automatically the scan on/off
+	 */
+	private void stopScan() {
+		mBLEAdapter.stopLeScan(this);
+		setProgressBarIndeterminateVisibility(false);
+		//Turn off scan for 5 sec
+		mHandler.postDelayed(mStartRun, 5000);
+	}
+	
+	private void startScan() {
+		//Scan for all periherals (can use UUID [] to only find certain)
+		mBLEAdapter.startLeScan(this);
+		setProgressBarIndeterminateVisibility(true);
+		//Turn on scan for 5 sec
+		mHandler.postDelayed(mStopRun, 5000);
+	}	
+	
+	
+	/*
+	 * LE Scan Callback
+	 */
+	@Override
+	public void onLeScan(BluetoothDevice device, int rssi, byte [] scanRecord) {
+		Log.i(TAG, "New BLE Device: " + device.getName() + " @ " + rssi);
+		
+		//Create a new beacon and then pass it to handler to update map
+		BLEBeacon beacon = new BLEBeacon(device.getAddress(), rssi);		
+		mHandler.sendMessage(Message.obtain(null, 0, beacon));
+		
+		//scanRecord holds raw data if needed
+	}
+	
+	
+	/*
+	 * Handler to pass scan results to main to update list view
+	 */
+	private Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			BLEBeacon beacon = (BLEBeacon) msg.obj;
+			mBeacons.put(beacon.getAddress(), beacon);
+			
+			//Clear then put new information into mAdapter list to display
+			mAdapter.setNotifyOnChange(false);
+			mAdapter.clear();
+			mAdapter.addAll(mBeacons.values());
+			mAdapter.notifyDataSetChanged();
+			
+		}
+	};
+	
+	
+	
+	/*
+	 * Custom List/ array adapter to display the found Beacons
+	 */
+	private static class BeaconListAdapter extends ArrayAdapter<BLEBeacon> {
+		
+		public BeaconListAdapter(Context context) {
+			super(context, 0);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if(convertView == null) {
+				convertView = LayoutInflater.from(getContext())
+						.inflate(R.layout.item_beacon_list, parent, false);
+			}
+			
+			BLEBeacon beacon = getItem(position);
+			
+			TextView addressView = (TextView) convertView.findViewById(R.id.text_address);
+			addressView.setText(beacon.getAddress());
+			
+			TextView rssiView = (TextView) convertView.findViewById(R.id.text_rssi);
+			rssiView.setText(String.format("%ddBm", beacon.getSignal()));
+			
+			return convertView;			
+		}		
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -83,23 +207,6 @@ public class MainActivity extends Activity implements IBeaconConsumer {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
 	}
 
 }
