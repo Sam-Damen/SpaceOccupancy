a2793988b11b2f5ffcaeebfe88446a1e263787f3
Sam Damen <damen.93@hotmail.com>
2014-10-12 14:19:27 +1000

    Cosmetic Update
    Changed app name and other cosmetic parts to look better

--- gen/com/example/bleproximity/R.java
+++ gen/com/example/bleproximity/R.java
@@ -10,6 +10,9 @@ package com.example.bleproximity;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int white=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,24 +27,26 @@ public final class R {
     public static final class drawable {
         public static final int ble=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int mainicon=0x7f020002;
+        public static final int mainiconbig=0x7f020003;
     }
     public static final class id {
-        public static final int auto_scan=0x7f08000f;
-        public static final int button1=0x7f080004;
-        public static final int button2=0x7f080005;
-        public static final int button3=0x7f080006;
-        public static final int button_publish=0x7f080002;
-        public static final int button_scan=0x7f080001;
-        public static final int clear_beacons=0x7f08000e;
-        public static final int container=0x7f080003;
-        public static final int editText1=0x7f08000a;
-        public static final int editText2=0x7f080009;
-        public static final int imageView1=0x7f080007;
-        public static final int listview=0x7f080000;
-        public static final int textView1=0x7f080008;
-        public static final int text_address=0x7f08000c;
-        public static final int text_num=0x7f08000b;
-        public static final int text_rssi=0x7f08000d;
+        public static final int auto_scan=0x7f09000f;
+        public static final int button1=0x7f090004;
+        public static final int button2=0x7f090005;
+        public static final int button3=0x7f090006;
+        public static final int button_publish=0x7f090002;
+        public static final int button_scan=0x7f090001;
+        public static final int clear_beacons=0x7f09000e;
+        public static final int container=0x7f090003;
+        public static final int editText1=0x7f09000a;
+        public static final int editText2=0x7f090009;
+        public static final int imageView1=0x7f090007;
+        public static final int listview=0x7f090000;
+        public static final int textView1=0x7f090008;
+        public static final int text_address=0x7f09000c;
+        public static final int text_num=0x7f09000b;
+        public static final int text_rssi=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_beacons=0x7f030000;
@@ -51,8 +56,8 @@ public final class R {
         public static final int item_beacon_list=0x7f030004;
     }
     public static final class menu {
-        public static final int beaconlist=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int beaconlist=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
         public static final int action_autoScan=0x7f050004;
@@ -94,10 +99,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
